04e0938f687bac393e2bedd4b8c981065d9a9061
Mike Chiloane <125856129+chiloanemike@users.noreply.github.com>
2025-02-06 19:08:54 +0200

    Animated the divs

--- app/components/Intro/index.tsx
+++ app/components/Intro/index.tsx
@@ -26,37 +26,42 @@ const Intro = () => {
 
     return (
         <AnimatedContainer>
-        <div className={` w-full text-white `}>
-            <div className="">
-                <p className="text-[2.2rem]  md:text-[3.5rem] leading-[60px] text-start font-medium">Mike Chiloane</p>
-                <p className="text-xl md:text-2xl font-[400]">Intermediate Java Developer</p>
-                <p className="text-[.94rem] mt-4 font-[200] text-white/80 md:max-w-[400px]">A proficient Java Software
-                    Engineer,
-                    leveraging
-                    industry best practices to transform innovative ideas into robust and scalable software
-                    solutions.</p>
-                <div className="flex space-x-4 mt-4">
-                    {
-                        socialLinks.map((link, index) => <SocialStack key={index} imagePath={link.imagePath}
-                                                                      link={link.link}/>)
-                    }
+            <div className={` w-full text-white `}>
+                <div className="">
+                    <p className="text-[2.2rem]  md:text-[3.5rem] leading-[60px] text-start font-medium">Mike
+                        Chiloane</p>
+                    <p className="text-xl md:text-2xl font-[400]">Intermediate Java Developer</p>
+                    <p className="text-[.94rem] mt-4 font-[200] text-white/80 md:max-w-[400px]">A proficient Java
+                        Software
+                        Engineer,
+                        leveraging
+                        industry best practices to transform innovative ideas into robust and scalable software
+                        solutions.</p>
+                    <div className="flex space-x-4 mt-4">
+                        {
+                            socialLinks.map((link, index) => <SocialStack key={index} imagePath={link.imagePath}
+                                                                          link={link.link}/>)
+                        }
+                    </div>
+
+                    <div className="mt-8 flex justify-start">
+                        <a href="/attachments/cv.doc" download="mike_cv.doc">
+                            <button
+                                className="bg-teal-400/20 rounded-full px-4 py-1 text-[1rem] items-center text-teal-300 font-[400] flex gap-2">
+                                <p>Download
+                                    CV</p><Image src="/download.svg" height={15} width={15} alt="download cv"/>
+                            </button>
+                        </a>
+                    </div>
+
+                    <div className="mt-16 flex flex-col space-y-5">
+                        {
+                            scrollReferences.map((ref, index) => <ScrollRef key={index} ref={ref}/>)
+                        }
+                    </div>
                 </div>
 
-                <div className="mt-8 flex justify-start">
-                    <button
-                        className="bg-teal-400/20 rounded-full px-4 py-1 text-[1rem] items-center text-teal-300 font-[400] flex gap-2"><p>Download
-                        CV</p><Image src="/download.svg" height={15} width={15}  alt="download cv"/>
-                    </button>
-                </div>
-
-                <div className="mt-8 flex flex-col space-y-5">
-                    {
-                        scrollReferences.map((ref, index) => <ScrollRef key={index} ref={ref}/>)
-                    }
-                </div>
             </div>
-
-        </div>
         </AnimatedContainer>
     )
 }
